8a39a364356ad906e1fbeb7ad3e15c9e00362cef
waitingzhao <wtz206@nyu.edu>
2023-11-09 00:13:29 -0500

    Merging the data from studentTerm and studentInfo for solving question for graduation_rate

---
+++ Graduation_Rate.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# open the data
+courseData = pd.read_csv("Course section info.csv")
+studentCourse = pd.read_csv("Student - course section info.csv")
+studentCareer = pd.read_csv("Student career info.csv")
+studentInfo = pd.read_csv("Student info.csv")
+studentTerm = pd.read_csv("Student term info.csv")
+
+#Merging the data from studentTerm and studentInfo
+joinedDataset = studentTerm.merge(studentInfo, on=["Fake ID"], how="left")
+selectedColumns = joinedDataset[["Fake ID"]]
+st.write(joinedDataset)
